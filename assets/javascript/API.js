7a996431aa8ae3147fa404bb4a951c8273f7dc84
John Sasser <cw.john.sasser@gmail.com>
2020-01-16 20:56:17 -0500

    moved the api code to the api.js file

---
+++ assets/javascript/API.js
@@ -0,0 +1,17 @@
+// google maps key without restrictions...
+let $googleMapsKey = "key=API_KEY" + "AIzaSyDyHXb9WjRwhERKpNkK8svc50vOr-YsUJw";
+let $input = $("#textarea2");
+
+// API call for google maps .js api;
+function getLocation() {
+	let mapsURL =
+		"https://maps.googleapis.com/maps/api/js?" +
+		googleMapsKey +
+		"&callback=initMap";
+	$.ajax({
+		url: mapsURL,
+		method: "GET"
+	}).then(function(res) {
+		console.log(res);
+	});
+}
