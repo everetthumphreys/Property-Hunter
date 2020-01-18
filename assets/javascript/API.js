6cbe35b650492fbba24b37cddcb01ba88b62b8c7
John Sasser <cw.john.sasser@gmail.com>
2020-01-18 13:35:28 -0500

    fixmerge

--- assets/javascript/API.js
+++ assets/javascript/API.js
@@ -2,7 +2,7 @@
 let $googleMapsKey = "key=API_KEY" + "AIzaSyDyHXb9WjRwhERKpNkK8svc50vOr-YsUJw";
 let $input = $("#textarea2");
 
-// API call for google maps .js api
+// API call for google maps  .js api
 function getLocation() {
 	let mapsURL =
 		"https://maps.googleapis.com/maps/api/js?" +
