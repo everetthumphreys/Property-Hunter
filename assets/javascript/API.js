b0441d8821a22aade9632a8f6ef67debb004dfc0
john chang <taichang751@gmail.com>
2020-01-18 12:43:57 -0500

    DONT COMMIT

--- assets/javascript/API.js
+++ assets/javascript/API.js
@@ -3,15 +3,17 @@ let $googleMapsKey = "key=API_KEY" + "AIzaSyDyHXb9WjRwhERKpNkK8svc50vOr-YsUJw";
 let $input = $("#textarea2");
 
 // API call for google maps .js api;
-function getLocation() {
-	let mapsURL =
-		"https://maps.googleapis.com/maps/api/js?" +
-		googleMapsKey +
-		"&callback=initMap";
-	$.ajax({
-		url: mapsURL,
-		method: "GET"
-	}).then(function(res) {
-		console.log(res);
-	});
+let getLocation = (name, address) => {
+    let mapsURL =
+        "https://maps.googleapis.com/maps/api/js?" +
+        googleMapsKey +
+        "&callback=initMap";
+    $.ajax({
+        url: mapsURL,
+        method: "GET"
+    }).then(res => {
+        console.log(res);
+    }).catch(err => {
+        console.log(err)
+    });
 }
---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+(() => {
+    //user validation logic
+})();
+
+
+
+document.getElementById("sender").addEventListener('click', e => {
+    let textvalue = document.getElementById("checklist").value;
+    let templateValue = document.getElementById("template").value;
+    // getLocation(checklistvalue, templatevalue)
+
+
+
+
+    // console.log(textvalue, templateValue)
+});
+localStorage
