b444425f91d36d125e906b324508c56c1ca71839
XikkeN <e.humphreys@tricentis.com>
2020-01-21 18:52:07 -0500

    reworked the form slightly and added notes to the JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,23 +7,23 @@ $(document).ready(function () {
         let parameterInput = $('#parameter').val(); //this is the selected parameter
         let priorityInput = $('input[name="priority"]:checked').val(); //this is the selected priority
         let collectionItemTemplate = $('<p>').attr('class', 'collection-item' + ' ' + priorityInput).text(parameterInput); //create a <p> with parameterInput
-        let iconText = '';
-        let pointValue = 0;
-        if (priorityInput == 'low') {
-            iconText = 'panorama_fish_eye';
-            pointValue = 1;
+        let iconText = ''; //set iconText to an empty string
+        let pointValue = 0; //set point value to 0
+        if (priorityInput == 'low') { 
+            iconText = 'panorama_fish_eye'; //sets the icon to low 
+            pointValue = 1; //sets the point value to one
         } else if (priorityInput == 'medium') {
-            iconText = 'favorite_border';
-            pointValue = 2;
+            iconText = 'favorite_border'; //sets the icon to medium
+            pointValue = 2; //sets the points to 2
         } else if (priorityInput == 'high') {
-            iconText = 'favorite';
-            pointValue = 3;
+            iconText = 'favorite'; //sets the icon to high
+            pointValue = 3; //sets the points to three
         }
-        let iconElement = $('<i>').attr('class', 'material-icons right').text(iconText);
-        collectionItemTemplate.append(iconElement);
-        templateScorePoints += pointValue;
-        $('.collection-template').append(collectionItemTemplate);
-        renderCheckList(parameterInput, iconText, pointValue);
+        let iconElement = $('<i>').attr('class', 'material-icons right').text(iconText); //builds the icon
+        collectionItemTemplate.append(iconElement); //appends the icon to collectionItemTemplate
+        templateScorePoints += pointValue; //adds points to the templateScorePoints
+        $('.collection-template').append(collectionItemTemplate); //appends the collectionItemTemplate to the page
+        renderCheckList(parameterInput, iconText, pointValue); //passes parameterInput, iconText, pointValue to the renderChecklist function
     };
 
     function renderCheckList(parameterInput, iconText, pointValue) {
@@ -31,9 +31,9 @@ $(document).ready(function () {
         let checkListLabel = $('<label>');
         let checkListInput = $('<input>').attr('type', 'checkbox').attr('value', pointValue);
         let checklistSpan = $('<span>').text(parameterInput);
-        let icon = $('<i>').attr('class', 'material-icons right').text(iconText);
-        checkListParagraph.append(checkListLabel.append(checkListInput).append(checklistSpan).append(icon));
-        $('.collection-checklist').append(checkListParagraph);
+        let icon = $('<i>').attr('class', 'material-icons right').text(iconText); //lines 30-34 are building the checklist
+        checkListParagraph.append(checkListLabel.append(checkListInput).append(checklistSpan).append(icon)); //appends everything each other in the proper order
+        $('.collection-checklist').append(checkListParagraph); //appends things to the page
     };
 
     $('.submit-template').on('click', function (event) {
@@ -41,5 +41,5 @@ $(document).ready(function () {
         renderTemplate();
     })
     //.each class checkbox onclick grab the state (checked or unchecked) then add or subtract based on the state
-    //in the same function 
+    //in the same function then calculate the score.
 });
