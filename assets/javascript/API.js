55193d2c19cddfe19cdf6ce7b360071b2595953e
John Sasser <cw.john.sasser@gmail.com>
2020-01-22 18:37:10 -0500

    work in progress, proving more difficult than i thought it would have been lol

--- assets/javascript/API.js
+++ assets/javascript/API.js
@@ -42,6 +42,47 @@ function initMap() {
 		handleLocationError(false, infoWindow, map.getCenter());
 	}
 }
+function initDirections(currentLat, currentLng) {
+	var directionsService = new google.maps.DirectionsService();
+	var directionsRenderer = new google.maps.DirectionsRenderer();
+	var haight = new google.maps.LatLng(37.7699298, -122.4469157);
+	var oceanBeach = new google.maps.LatLng(
+		37.7683909618184,
+		-122.51089453697205
+	);
+	// console.log(oceanBeach);
+	var mapOptions = {
+		zoom: 12,
+		center: haight
+	};
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	directionsRenderer.setMap(map);
+
+	function calcRoute() {
+		// var selectedMode = document.getElementById("mode").value;
+		let selectedMode = $("input")
+			.attr("travelMode")
+			.firstChild()
+			.val();
+		console.log(selectedMode);
+		var request = {
+			origin: haight,
+			destination: oceanBeach,
+			travelMode: "DRIVING"
+			// travelMode: $("#mode")
+			// 	.children(2)
+			// 	.child(2)
+			// 	.val()
+		};
+		console.log(google.maps);
+		directionsService.route(request, function(response, status) {
+			if (status == "OK") {
+				directionsRenderer.setDirections(response);
+			}
+		});
+	}
+	calcRoute();
+}
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.setPosition(pos);
@@ -53,23 +94,24 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 	infoWindow.open(map);
 }
 
+// function directions(address) {
+// 	let origin = currentLat;
+// 	// currentLng;
+// 	let destination = address.replace(/ /g, "+");
+// 	let directionURL =
+// 		"https://maps.googleapis.com/maps/api/directions/json?origin=" +
+// 		origin +
+// 		"&destination=" +
+// 		destination +
+// 		"&key=" +
+// 		googleMapsKey;
+// 	console.log("origin: " + origin, "dest: " + destination);
+// 	console.log(directionURL);
+// }
+
 // click event for the address to populate directions in the map;  need to append to the div and replace the initial map with a .hide class switch;
 $submit.on("click", function() {
 	let address = $input.val();
-	directions(address);
-
-	function directions(address) {
-		let directionURL =
-			"https://maps.googleapis.com/maps/api/directions/json?origin=" +
-			origin +
-			"&destination=" +
-			destination +
-			"&key=" +
-			googleMapsKey;
-		let origin = currentLat,
-			currentLng;
-		let destination = address;
-		console.log("origin: " + origin, "dest: " + destination);
-	}
-	directions();
+	// directions(address);
+	initDirections(address);
 });
