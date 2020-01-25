6dc415c8f5ea01fbcfa8c98c7f614d95c6399da8
john chang <taichang751@gmail.com>
2020-01-25 09:50:45 -0500

    added local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,70 +1,214 @@
 $(document).ready(function() {
+    $(".collection-checklist").empty(); // clear default checklist
+    apartmentList(); //Load appartment list
+    theTemplate(); //load parameter and checklist
 
-    let templateScorePoints = 0;
-    let checkListScorePoints = 0;
+    //Add property button clicked
+    $("#submit-template").on("click", function(event) {
+        event.preventDefault();
 
-    //localStorage items
-    // let localStoragePrefs = JSON.parse(localStorage.getItem("prefs")) || [];
-    let localStoragePrefs = JSON.parse(localStorage.getItem("prefs")) || [];
-
-
-    function renderTemplate() {
-        let parameterInput = $('#parameter').val(); //this is the selected parameter
-        let priorityInput = $('input[name="priority"]:checked').val(); //this is the selected priority
-        let collectionItemTemplate = $('<p>').attr('class', 'collection-item' + ' ' + priorityInput).text(parameterInput); //create a <p> with parameterInput
-        let iconText = ''; //set iconText to an empty string
-        let pointValue = 0; //set point value to 0
-        if (priorityInput == 'low') {
-            iconText = 'panorama_fish_eye'; //sets the icon to low 
-            pointValue = 1; //sets the point value to onex
-        } else if (priorityInput == 'medium') {
-            iconText = 'favorite_border'; //sets the icon to medium
-            pointValue = 2; //sets the points to 2
-        } else if (priorityInput == 'high') {
-            iconText = 'favorite'; //sets the icon to high
-            pointValue = 3; //sets the points to three
+        //check if parameter is not empty
+        if ($("#parameter").val() !== "") {
+            newRenderTmp();
         }
-        let iconElement = $('<i>').attr('class', 'material-icons right').text(iconText); //builds the priority icon
-        let templateItemCancel = $('<i>').attr('class', 'material-icons left cancel').text("cancel"); //builds the cancel icon
-        collectionItemTemplate.append(templateItemCancel).append(iconElement); //appends the icon to collectionItemTemplate
+    });
 
-        localStoragePrefs.push({
-            text: parameterInput,
-            value: pointValue
-        }); //update the array in our js file that maps to local storage
+    //Add property button clicked
+    $("#p-button").on("click", function(event) {
+        event.preventDefault();
 
-        localStorage.setItem("prefs", JSON.stringify(localStoragePrefs)); //because we updated the array that maps to local storage, we need to go ahead and reset localStorage's "prefs" item too
-        templateScorePoints += pointValue; //adds points to the templateScorePoints
-        debugger;
-        $('.collection-template').append(collectionItemTemplate); //appends the collectionItemTemplate to the page
-        renderCheckList(parameterInput, iconText, pointValue); //passes parameterInput, iconText, pointValue to the renderChecklist function
-    };
-
-    function renderCheckList(parameterInput, iconText, pointValue) {
-        let checkListParagraph = $('<p>').attr('class', 'collection-item');
-        let checkListLabel = $('<label>');
-        let checkListInput = $('<input>').attr('type', 'checkbox').attr('value', pointValue);
-        let checklistSpan = $('<span>').text(parameterInput);
-        let icon = $('<i>').attr('class', 'material-icons right').text(iconText); //lines 30-34 are building the checklist
-        checkListParagraph.append(checkListLabel.append(checkListInput).append(checklistSpan).append(icon)); //appends everything each other in the proper order
-        $('.collection-checklist').append(checkListParagraph); //appends things to the page
-    };
-
-    $('#submit-template').on('click', function(event) {
-            event.preventDefault();
-            renderTemplate();
-        })
-        //.each class checkbox onclick grab the state (checked or unchecked) then add or subtract based on the state
-        //in the same function then calculate the score.
+        if ($("#checkList").val() !== "") {
+            data = {
+                property: $("#checkList").val(),
+                note: $("#notes").val(),
+                address: $("#address-input").val(),
+                trasit: $('input[name="transit"]:checked').val(),
+                preferences: buildCheckedPreferences()
+            };
+            var prop = localStorage.getItem("properties");
+            if (prop) {
+                var another = [];
+                another = JSON.parse(prop);
+                data.id = another.length + 1;
+                another.push(data);
+                localStorage.setItem("properties", JSON.stringify(another));
+                apartmentList();
+            } else {
+                var another = [];
+                data.id = 1;
+                another.push(data);
+                localStorage.setItem("properties", JSON.stringify(another));
+                apartmentList();
+            }
+        }
+    });
 });
 
+let templateScorePoints = 0;
+let checkListScorePoints = 0;
 
-//localstorage model for saved properties:
-//each new Location:
-// {
-//     name: "Buckhead Lofts",
-//     prefs: [{text: "close to work", value: 3}, {text: "dog park", value: 1}],
-//     totalScore: 45
-// }
+//BUild up the paramater
+function newRenderTmp() {
+    var result = [];
+    var data = {
+        param: $("#parameter").val(),
+        priority: $('input[name="priority"]:checked').val()
+    };
+    var prior = setPriorityIcon(data.priority);
+    var p = localStorage.getItem("parameters");
+    if (p) {
+        result = JSON.parse(p);
+        var newData = {
+            text: data.param,
+            value: prior.pointValue
+        };
+        newData.id = result.length + 1;
+        result.push(newData);
+        localStorage.setItem("parameters", JSON.stringify(result));
+        $(".collection-checklist").empty();
+        theTemplate();
+    } else {
+        var newData = {
+            text: data.param,
+            value: prior.pointValue
+        };
+        newData.id = 1;
+        result.push(newData);
+        localStorage.setItem("parameters", JSON.stringify(result));
+        $(".collection-checklist").empty();
+        theTemplate();
+    }
+}
 
-//localStorage could always have the current "high score" which is all the current prefs scores added together
+// THis module renders the parameter template
+function theTemplate() {
+    var data = JSON.parse(localStorage.getItem("parameters"));
+    $(".collection-template").empty();
+    if (data) {
+        for (const iterator of data) {
+            let collectionItemTemplate = $("<p>")
+                .attr("class", "collection-item" + " " + iterator.value)
+                .text(iterator.text);
+            let iconElement = $("<i>")
+                .attr("class", "material-icons right")
+                .text(buildIconSet(iterator.value)); //builds the priority icon
+            let templateItemCancel = $("<i>")
+                .attr("onclick", "reove(" + iterator.id + ")")
+                .attr("class", "material-icons left cancel")
+                .text("cancel"); //builds the cancel icon
+            collectionItemTemplate.append(templateItemCancel).append(iconElement); //appends the icon to collectionItemTemplate
+            templateScorePoints += iterator.value; //adds points to the templateScorePoints
+            $(".collection-template").append(collectionItemTemplate); //appends the collectionItemTemplate to the pag
+            renderCheckList(iterator.text, iterator.value, iterator.value);
+        }
+    }
+}
+// This module will check for icon type
+function setPriorityIcon(req) {
+    var res = {};
+    switch (req) {
+        case "low":
+            res.iconText = "panorama_fish_eye";
+            res.pointValue = 1;
+            break;
+
+        case "medium":
+            res.iconText = "favorite_border";
+            res.pointValue = 2;
+            break;
+
+        case "high":
+            res.iconText = "favorite";
+            res.pointValue = 3;
+            break;
+
+        default:
+            res.error = "true";
+            break;
+    }
+    return res;
+}
+
+//Build the Icon set
+function buildIconSet(val) {
+    var res = "";
+    switch (val) {
+        case 1:
+            res = "panorama_fish_eye";
+            break;
+
+        case 2:
+            res = "favorite_border";
+            break;
+
+        case 3:
+            res = "favorite";
+            break;
+
+        default:
+            res = "error";
+            break;
+    }
+    return res;
+}
+
+// This will populate the property checklist
+function renderCheckList(parameterInput, iconText, pointValue) {
+    let checkListParagraph = $("<p>").attr("class", "collection-item");
+    let checkListLabel = $("<label>");
+    let checkListInput = $("<input>")
+        .attr("type", "checkbox")
+        .attr("name", "my-check")
+        .attr("value", pointValue);
+    let checklistSpan = $("<span>").text(parameterInput);
+    let icon = $("<i>")
+        .attr("data-id", pointValue)
+        .attr("class", "material-icons right")
+        .text(buildIconSet(iconText)); //lines 30-34 are building the checklist
+    checkListParagraph.append(
+        checkListLabel
+        .append(checkListInput)
+        .append(checklistSpan)
+        .append(icon)
+    ); //appends everything each other in the proper order
+    $(".collection-checklist").append(checkListParagraph); //appends things to the page
+}
+
+// This module will help build all preference checkboxe
+function buildCheckedPreferences() {
+    var res = [];
+    $("input:checkbox[name=my-check]:checked").each(function() {
+        res.push($(this).val());
+    });
+    console.log(res);
+    return res;
+}
+
+// This module will generate the apartment list
+function apartmentList() {
+    var prop = JSON.parse(localStorage.getItem("properties"));
+    $("#apart").empty();
+    if (prop) {
+        for (const iterator of prop) {
+            let apartment = $("<a>")
+                .attr("href", "#!")
+                .attr("class", "collection-item")
+                .text(iterator.property);
+            $("#apart").append(apartment);
+        }
+    }
+}
+
+//Remove the deleted parameter
+function reove(id) {
+    var data = JSON.parse(localStorage.getItem("parameters"));
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+            data.splice(i, 1);
+            localStorage.setItem("parameters", JSON.stringify(data));
+            $(".collection-checklist").empty();
+            theTemplate();
+
+        }
+    }
+}
